a215b398e39a9ea7857ed12e794ccb86d2d5b8cc
Rama <gudrms2538@gmail.com>
2025-03-16 23:09:31 +0900

    Initial Commit

---
+++ SwiftUI-Gigang/SwiftUI_GigangApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GigangApp.swift
+//  SwiftUI-Gigang
+//
+//  Created by Rama on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GigangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-GigangTests/SwiftUI_GigangTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GigangTests.swift
+//  SwiftUI-GigangTests
+//
+//  Created by Rama on 3/16/25.
+//
+
+import Testing
+@testable import SwiftUI_Gigang
+
+struct SwiftUI_GigangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
