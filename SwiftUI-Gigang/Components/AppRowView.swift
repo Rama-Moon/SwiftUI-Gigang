7419ff9a72d21ab2355d6c8639a494c8c7717cba
jamin <jamin@10-141-43-20.0103.pos.kr.iosda.org>
2025-03-25 15:25:34 +0900

    [Add] Add files, essets

---
+++ SwiftUI-Gigang/Components/AppRowView.swift
@@ -0,0 +1,18 @@
+//
+//  AppRowView.swift
+//  SwiftUI-Gigang
+//
+//  Created by jamin on 3/24/25.
+//
+
+import SwiftUI
+
+struct AppRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppRowView()
+}
---
+++ SwiftUI-Gigang/View/AppsView.swift
@@ -0,0 +1,18 @@
+//
+//  AppsView.swift
+//  SwiftUI-Gigang
+//
+//  Created by jamin on 3/24/25.
+//
+
+import SwiftUI
+
+struct AppsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppsView()
+}
