ce8e2435e336fbfd0e276f1da291a60fe5332f6e
jamin <jamin@10-141-43-20.0103.pos.kr.iosda.org>
2025-03-25 16:03:49 +0900

    [Feat] AppsView Layout (except making CustomHeaderView)

--- SwiftUI-Gigang/View/AppsView.swift
+++ SwiftUI-Gigang/View/AppsView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct AppsView: View {
+    let rows: [GridItem] = Array(repeating: GridItem(.fixed(76), spacing: 0), count: 3)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 0) {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 10) {
+                            ForEach(0..<2, id: \.self) { index in
+                                AppCardView()
+                            }
+                        }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .background(.red)
+                        .padding(.horizontal, 20)
+                    }
+                    
+                    Group {
+                        Divider()
+                            .padding(.top, 32)
+                        
+                        HStack {
+                            Text("iPhone Essentials")
+                                .font(.title)
+                                .bold()
+                                .padding(.top, 6)
+                            
+                            Spacer()
+                            
+                            Text("See All")
+                                .font(.title2)
+                                .padding(.top, 6)
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: rows, spacing: 10) {
+                            ForEach(0..<6) { index in
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(Color.gray)
+                                    .frame(width: 353)
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                }
+                .background(.blue)
+            }
+            .navigationTitle("Apps")
+            .navigationBarTitleDisplayMode(.automatic)   
+        }
     }
 }
 
