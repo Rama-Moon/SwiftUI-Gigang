18080cb19d484749c11cdfd9f4665c1132fa2995
jamin <jamin@10-141-43-20.0103.pos.kr.iosda.org>
2025-03-25 16:06:10 +0900

    [Feat] Set MainTabView as initial view

--- SwiftUI-Gigang/View/MainTabView.swift
+++ SwiftUI-Gigang/View/MainTabView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            AppsView()
+                .tabItem {
+                    Image(systemName: "square.stack.3d.up.fill")
+                    Text("Apps")
+                }
+        }
     }
 }
 
