46ae4775aeff8ef82afdefa8025661fa6f78ff47
jamin <jamin@jaminui-MacBookAir.local>
2025-03-30 13:39:03 +0900

    [Refactor] NavigationVeiw 분리

--- SwiftUI-Gigang/View/AppsView.swift
+++ SwiftUI-Gigang/View/AppsView.swift
@@ -11,58 +11,9 @@ struct AppsView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVStack(alignment: .leading, spacing: 0) {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack(spacing: 10) {
-                            ForEach(0..<5, id: \.self) { index in
-                                AppCardView()
-                                    .scrollTargetLayout()
-                            }
-                        }
-                        .padding(.horizontal, 20)
-                    }
-                    .scrollTargetBehavior(.viewAligned)
-                    
-                    Group {
-                        Divider()
-                            .padding(.top, 32)
-                        
-                        HStack {
-                            Text("iPhone Essentials")
-                                .font(.title)
-                                .bold()
-                                .padding(.top, 6)
-                            
-                            Spacer()
-                            
-                            Text("See All")
-                                .font(.title3)
-                                .padding(.top, 6)
-                                .foregroundStyle(.blue)
-                        }
-                    }
-                    .padding(.horizontal, 20)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack(spacing: 10) {
-                            ForEach(0..<6, id: \.self) { columnIndex in
-                                VStack(spacing: 14) {
-                                    ForEach(0..<3, id: \.self) { rowIndex in
-                                        AppRowView(style: .grid)
-                                            .frame(width: UIScreen.main.bounds.width - 40)
-                                    }
-                                }
-                                .padding(.top, 5)
-                                .scrollTargetLayout()
-                            }
-                        }
-                        .padding(.horizontal, 20)
-                    }
-                    .scrollTargetBehavior(.viewAligned)                }
-            }
-            .navigationTitle("Apps")
-            .navigationBarTitleDisplayMode(.automatic)
+            AppsContentView()
+                .navigationTitle("Apps")
+                .navigationBarTitleDisplayMode(.automatic)
         }
     }
 }
