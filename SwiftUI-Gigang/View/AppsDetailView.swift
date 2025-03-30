3a20dbe3ae3161d168fa6454fc1ec1503b9f6734
jamin <jamin@jaminui-MacBookAir.local>
2025-03-30 20:12:35 +0900

    [Feat] AppsDetailView, FeaturedAppView UI 구현 및 데이터 바인딩

--- SwiftUI-Gigang/View/AppsDetailView.swift
+++ SwiftUI-Gigang/View/AppsDetailView.swift
@@ -8,11 +8,76 @@
 import SwiftUI
 
 struct AppsDetailView: View {
+    let app: AppModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    HStack {
+                        Image(.imgAppIcon)
+                            .resizable()
+                            .frame(width: 118, height: 118)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text(app.name)
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            
+                            Text(app.developer)
+                                .font(.headline)
+                                .foregroundStyle(.secondary)
+                                .padding(.top, 3)
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Button(action: { }) {
+                                    Text("Get")
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal, 20)
+                                        .padding(.vertical, 4)
+                                }
+                                .background(.blue)
+                                .clipShape(.capsule)
+                                
+                                Spacer()
+                                
+                                Button(action: { }) {
+                                    Image(systemName: "square.and.arrow.up")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 24, height: 24)
+                                    
+                                }
+                            }
+                        }  
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        Divider()
+                            .padding(.horizontal, 20)
+                        LazyHStack {
+                            ForEach(0..<7) { _ in
+                                AppMetaItemView()
+                                    .frame(width: 104)
+                                Divider()
+                                    .frame(width: 0.5, height: 40)
+                                    .background(.placeholder)
+                            }
+                            
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                    .padding(.top, 20)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    AppsDetailView()
+    AppsDetailView(app: AppModel(name: "dd", category: "dd", developer: "dd", iconName: "imgAppIcon"))
 }
